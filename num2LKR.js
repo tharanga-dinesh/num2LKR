37e6d9a87b70b75df6a8ffb8184ef62842c1ab25
tharanga-dinesh <dinesh1official@gmail.com>
2018-08-06 20:46:54 +0530

    js file created

---
+++ num2LKR.js
@@ -0,0 +1,15 @@
+/**
+ * @summary     num2LKR
+ * @description A jQuery plugin to convert numbers or amount to Sri Lanka Rupees
+ * @version     1.0.0
+ * @file num2LKR.js
+ * @author tharanga-dinesh(dinesh1official@gmail.com)
+ */
+
+ (function($){
+    $.fn.extend({
+        num2LKR : function(options){
+
+        }
+    });
+ }(jQuery))
